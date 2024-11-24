f1d24f0f57f90b3b75f38aed0c51e19d28e3de59
taha <tahamzahry@gmail.com>
2024-11-24 13:40:25 +0330

    Create RasmioProvider.php

---
+++ src/Providers/RasmioProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Taha\Rasmio\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class InspirationProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+    }
+}
