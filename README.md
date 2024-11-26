d036a8bebed999d920eed5b89c87f1920db54d58
taha <tahamzahry@gmail.com>
2024-11-26 13:31:01 +0330

    ...

---
+++ README.md
@@ -0,0 +1,30 @@
+# Rasmio - Laravel Package for Managing RasmiO Tasks
+
+[![Version](https://img.shields.io/badge/version-1.0.0-blue.svg)](https://packagist.org/packages/tahamazaheri/rasmio)
+[![License](https://img.shields.io/badge/license-MIT-green.svg)](https://opensource.org/licenses/MIT)
+
+The `rasmio` package is a powerful tool for managing **RasmiO** tasks in Laravel. It helps you manage these tasks effortlessly within your Laravel project.
+
+## Installation
+
+To install the package, run the following command in your terminal:
+
+```bash
+composer require tahamazaheri/rasmio
+
+
+## after Installation
+
+
+To install the package, run the following command in your terminal:
+
+```bash
+php artisan vendor:publish --tag=rasmio --force
+
+
+### Key Points:
+1. **Clear instructions:** It includes clear instructions on installation, setup, and usage.
+2. **Commands & Development:** Describes the Artisan commands and how others can contribute.
+3. **License Information:** Mentions the MIT License and links to the LICENSE file for more details.
+
+Feel free to adjust any parts to suit the specifics of your package or project!
--- src/RasmioServiceProvider.php
+++ src/RasmioServiceProvider.php
@@ -13,18 +13,36 @@ class RasmioServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        // // انتشار فایل‌های پیکربندی، ویوها و روت‌ها
-        // $this->mergeConfigFrom(
-        //     __DIR__ . '/../config/Rasmio.php', 'Rasmio'
-        // );
+       
+        // // انتشار روت‌ها
+        // $this->publishes([
+        //     __DIR__ . '/../src/Http/Controllers/Api' => app_path('Http/Controllers/Api'),
+        // ], 'controllers');
+        
+        // $this->publishes([
+        //     __DIR__ . '/../routes' => app_path('routes'),
+        // ], 'routes');
 
-        // انتشار روت‌ها
+        // $this->publishes([
+        //     __DIR__ . '/../src/Services' => app_path('Services'),
+        // ], 'Services');
+        
+          
+        // $this->publishes([
+        //     __DIR__ . '/../tests' => app_path('tests'),
+        // ], 'tests');
+
+
         $this->publishes([
-            __DIR__ . '/../src/Http/Controllers/Api' => app_path('Http/Controllers'),
-        ], 'controllers');
+            __DIR__ . '/../src/Http/Controllers/Api' => app_path('Http/Controllers/Api'),
+            __DIR__ . '/../routes' => base_path('routes'),
+            __DIR__ . '/../src/Services' => app_path('Services'),
+            __DIR__ . '/../tests' => base_path('tests'),
+        ], 'rasmio');  
+
         
         
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        // $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
 
 
     }
